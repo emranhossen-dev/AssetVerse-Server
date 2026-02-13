5bf2ed25777289459c41c07296eee61ae1b3e8ee
emran40989 <emran40989@gmail.com>
2026-02-14 02:41:29 +0600

    basic structure added for backend

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.port || 3000
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res)=>{
+    res.send("Hello Developers")
+})
+
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`)
+})
